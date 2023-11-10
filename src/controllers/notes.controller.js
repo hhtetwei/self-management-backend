da5df56bf98919ba08f5bc0a957ff1b3d27e6a57
hhtetwei <hhtet263510@gmail.com>
2023-11-10 15:43:49 +0630

    Note Done

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -2,38 +2,66 @@ const { NOTE_ERRORS } = require('../constants/errors.constants')
 const noteService = require('../services/notes.services')
 
 const notesController = {
-    getNotes: async (req, res, next) => {
-        try {
-            const notes = await noteService.getNotes(req.query)
+  getNotes: async (req, res, next) => {
+    try {
+      const notes = await noteService.getNotes({ query: req.query })
 
-            return res.status(201).json({
-                data: notes
-            })
-        }catch(err){
-            next(err)
-        }
-      
-    },
+      return res.status(201).json({
+        data: notes,
+      })
+    } catch (err) {
+      next(err)
+    }
+  },
 
-    getNote: async (req, res, next) => {
-        try {
-            const note = await noteService.getNote(req.params.id)
+  getNote: async (req, res, next) => {
+    try {
+      const note = await noteService.getNote(req.params.id)
 
-            if (!note) {
-                return NOTE_ERRORS.NOT_FOUND
-            }
+      if (!note) {
+        return NOTE_ERRORS.NOT_FOUND
+      }
 
-            return res.status(201).json({
-                data: note
-            })
-        } catch (err) {
-            next(err)
-        }
-    },
+      return res.status(201).json({
+        data: note,
+      })
+    } catch (err) {
+      next(err)
+    }
+  },
 
+  createNote: async (req, res, next) => {
+    try {
+      const notes = await noteService.createNote(req.body)
 
+      const payload = { ...req.body }
+
+      return res.status(201).json({ data: notes })
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  editNote: async (req, res, next) => {
+    try {
+      const notes = await noteService.editNote(req.params.id, req.body)
+
+      return res.status(201).json({ data: notes })
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  deleteNote: async (req, res, next) => {
+    try {
+      const notes = await noteService.deleteNote(req.params.id)
+      return res.status(201).json({ message: 'note is successfully deleted' })
+    } catch (err) {
+      next(err)
+    }
+  },
 }
 
 module.exports = {
-  notesController
+  notesController,
 }
---
+++ src/services/notes.services.js
@@ -0,0 +1,49 @@
+const { NOTE_ERRORS } = require('../constants/errors.constants')
+const noteModel = require('../models/notes.models')
+
+const noteService = {
+  getNotes: async ({ query }) => {
+    const { sort } = query
+
+    let filter = {}
+
+    const page = query.page * 1 || 1
+    const limit = query.limit === 0 ? query.limit : query.limit * 1 || 10
+    const skip = (page - 1) * limit
+
+    const [data, count] = await Promise.all([
+      noteModel.find(filter).limit(+limit).skip(+skip).sort(sort),
+      noteModel.find(filter).countDocuments(),
+    ])
+
+    return { data, count }
+  },
+
+  getNote: async (id) => {
+    const note = await noteModel.findById(id)
+
+    if (!note) {
+      return NOTE_ERRORS.NOT_FOUND
+    }
+
+    return note
+  },
+
+  createNote: async ({ payload }) => {
+    const note = await noteModel.create(payload)
+
+    return note
+  },
+
+  deleteNote: async (id) => {
+    const note = await noteModel.findByIdAndDelete(id)
+    if (!note) {
+      return NOTE_ERRORS.NOT_FOUND
+    }
+    return note
+  },
+}
+
+module.exports = {
+  noteService,
+}
